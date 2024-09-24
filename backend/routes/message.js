0339a36e01e7c5c9dae70f6fa45eda512eb85684
Minghao1999 <m_sun11@u.pacific.edu>
2024-09-23 20:33:18 -0700

    Auto-commit JSX on save for 227

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -4,4 +4,4 @@ const Message = require('../models/Message')
 const axios = require('axios')
 const {response} = require("express")
 
-router.get('/', async (request))
+router.get('/', async (request,response))
