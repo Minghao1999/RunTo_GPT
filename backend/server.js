f34134c0f67071c0bb44f14007af7d1a724bec55
Minghao1999 <m_sun11@u.pacific.edu>
2024-09-23 20:27:02 -0700

    Auto-commit JSX on save for 227

--- backend/server.js
+++ backend/server.js
@@ -11,4 +11,7 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(process.env.MON)
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
