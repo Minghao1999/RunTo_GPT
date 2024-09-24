fff97ebf1195fbebce86f7d3721cc7f626c7161e
Minghao1999 <m_sun11@u.pacific.edu>
2024-09-23 20:24:47 -0700

    Auto-commit JSX on save for 227

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv')
 
-dotenv
+dotenv.config()
+
+const app = express
+const PORT = process.env.PORT || 3000
