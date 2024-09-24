d3631f82155aded15bff3f44a409316545b1b331
Minghao1999 <m_sun11@u.pacific.edu>
2024-09-23 20:28:26 -0700

    Auto-commit JSX on save for 227

--- backend/server.js
+++ backend/server.js
@@ -17,3 +17,5 @@ mongoose.connect(process.env.MONGODB_URI, {
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('MongoDB connection error:', err))
+
+const messageRoutes = require('./routes/message')
