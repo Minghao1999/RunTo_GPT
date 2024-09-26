21f5a3373846eb4a26a0bd35fd55dcf21282f450
Minghao1999 <m_sun11@u.pacific.edu>
2024-09-25 19:52:43 -0700

    Auto-commit JSX on save for 227

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -38,7 +38,7 @@ router.post('/', async (request,response)=>{
 
 const getBotResponse = async (userInput) =>{
     try{
-        const response = await axios.post('https://api.openai.com/v1/chat')
+        const response = await axios.post('https://api.openai.com/v1/chat/completions')
     }
 }
 
