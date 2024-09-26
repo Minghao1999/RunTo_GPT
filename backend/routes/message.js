dc5e4be216ee2a32fe6fd6f2fa089906060e5faf
Minghao1999 <m_sun11@u.pacific.edu>
2024-09-25 19:50:21 -0700

    Auto-commit JSX on save for 227

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -29,7 +29,7 @@ router.post('/', async (request,response)=>{
             await botMessage.save()
             response.json(botMessage)
         }else {
-            res.json(newMessage)
+            response.json(newMessage)
         }
     }catch (err){
         response
